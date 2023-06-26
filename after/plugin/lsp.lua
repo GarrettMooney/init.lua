ba33d6f16297c24f561c09f338f56988b1594a2b
Garrett Mooney <garrettrmooney@gmail.com>
2023-06-26 14:07:59 -0400

    add placeholder navigation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,12 +15,19 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  -- trigger completion menu
+  ['<C-Space>'] = cmp.mapping.complete(),
+  -- navigate between snippet placeholders
+  ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+  -- navigate between suggestions
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  -- confirm selection
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
